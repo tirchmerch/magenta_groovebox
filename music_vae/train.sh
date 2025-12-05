5822fa3ae3d2ee75b2835195317e75beaa7ed5cb
tirchmerch <pmtirch@gmail.com>
2025-12-05 00:23:42 -0500

    fixed typo

--- music_vae/train.sh
+++ music_vae/train.sh
@@ -1,4 +1,34 @@
 #!/bin/bash
+#SBATCH --account=sdbarton
+#SBATCH --partition=quick
+
+DIR=$1
+PARAMS_OFFSET=$2
+
+PARAMS_FILE="${DIR}/params"
+RESULTS_FILE="${DIR}/results"
+RUNLOG_FILE="${DIR}/runlog"
+
+if [ -z "$PARAMS_OFFSET" ]
+then
+    PARAMS_OFFSET=0
+fi
+
+if [ ! -d "$DIR" -o ! -f "$PARAMS_FILE" ]
+then
+    echo "Usage: $0 DIR [PARAMS_OFFSET]"
+    echo "where DIR is a directory containing a file 'params' with the parameters."
+    exit 1
+fi
+
+ARAMS_ID=$(( $SLURM_ARRAY_TASK_ID + $PARAMS_OFFSET ))
+JOB_NAME="${SLURM_ARRAY_JOB_ID}_${SLURM_ARRAY_TASK_ID}"
+
+echo "$PARAMS_ID|$JOB_NAME|$SLURM_SUBMIT_DIR" >> $RUNLOG_FILE
+
+PARAMS=$(tail -n +${PARAMS_ID} ${PARAMS_FILE} | head -n 1)
+
+echo "*** TRAIN ***"
 
 start_seconds = $SECONDS
 

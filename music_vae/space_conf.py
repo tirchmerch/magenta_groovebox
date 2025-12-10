7cb44e8cea1976ef9c69f050c308992a0b0852f5
tirchmerch <pmtirch@gmail.com>
2025-12-10 11:34:59 -0500

    fixed ranges

--- music_vae/space_conf.py
+++ music_vae/space_conf.py
@@ -1,7 +1,7 @@
 import numpy as np
 
 space = {
-    'free_bits': np.arange(0, 256, step=16),
-    'max_beta': np.arange(0.05, 0.5, step=0.05),
+    'free_bits': np.arange(0, 272, step=16),
+    'max_beta': np.arange(0.05, 0.55, step=0.05),
     'batch_size': [512]
 }

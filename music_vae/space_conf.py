a13501d87d6df58af82867155b844d4b9dbaef9c
tirchmerch <pmtirch@gmail.com>
2025-12-10 11:31:19 -0500

    typo

--- music_vae/space_conf.py
+++ music_vae/space_conf.py
@@ -1,7 +1,7 @@
 import numpy as np
 
 space = {
-    'free_bits': np.arrange(0, 256, step=16),
+    'free_bits': np.arange(0, 256, step=16),
     'max_beta': np.arange(0.05, 0.5, step=0.05),
     'batch_size': 512
 }
